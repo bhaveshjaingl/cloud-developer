b201f26ed9b80a6cc1a25107ce120ce4d2757205
bhaveshjaingl <bhavesh.jain.gl@gmail.com>
2021-04-18 15:49:41 -0400

    changed config for chap4.3

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "bjaindbpg",
+    "password": "bjaindbpg",
+    "database": "bjaindbpg",
+    "host": "bhavesh-jain-postgres.c1fmcozf6sc1.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
